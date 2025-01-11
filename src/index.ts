d2e8afc137ccab92d95793c61bbbecabf97b7c1b
TakashiNishii <ygortn14@gmail.com>
2025-01-11 15:02:44 -0400

    :wrench: Express e os metodos HTTP

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,33 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World - tsx-watch");
 });
 
-app.get("/users", (req: Request, res: Response) => {
-  let usuarios = [
-    {
-      nome: "Ygor Takashi",
-      idade: 24,
-    },
-    { 
-      nome: "João",
-      idade: 30
-    }
-  ];
+let usuarios = [
+  {
+    nome: "Ygor Takashi",
+    idade: 24,
+  },
+  { 
+    nome: "João",
+    idade: 30
+  }
+];
 
+app.get("/users", (req: Request, res: Response) => {
   res.send(usuarios);
+})
+
+app.post("/users", (req: Request, res: Response) => {
+  let user = req.body;
+  usuarios.push(user);
+  res.send({
+    message: "Usuário cadastrado com sucesso",
+  })
 })
 
 app.listen(3000, () => {
