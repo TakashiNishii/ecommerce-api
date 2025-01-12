c898a3c7f780c5d893d4a3c054a25136cc216556
TakashiNishii <ygortn14@gmail.com>
2025-01-11 22:28:02 -0400

    :wrench: put and delete

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,56 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+// Registra o middleware para o express reconhecer o corpo da requisição em formato JSON
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World - tsx-watch");
 });
 
-let usuarios = [
-  {
-    nome: "Ygor Takashi",
-    idade: 24,
-  },
-  { 
-    nome: "João",
-    idade: 30
-  }
-];
+let id = 0;
+let usuarios: {id: number, nome: string, email: string}[] = [];
 
 app.get("/users", (req: Request, res: Response) => {
   res.send(usuarios);
 })
 
+app.get("/users/:id", (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let user = usuarios.find(user => user.id === userId);
+  res.send(user);
+})
+
+app.put("/users/:id", (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let user = usuarios.find(user => user.id === userId);
+
+  if(user){
+    user = req.body
+    res.send({
+      message: "Usuário atualizado com sucesso",
+    })
+  } 
+  if(!user) {
+    res.send({
+      message: "Usuário não encontrado",
+    })
+  }
+})
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let userList = usuarios.filter(user => user.id !== userId);
+  usuarios = userList;
+
+  res.send({
+    message: "Usuário deletado com sucesso",
+  })
+})
+
 app.post("/users", (req: Request, res: Response) => {
   let user = req.body;
+  user.id = ++id;
   usuarios.push(user);
   res.send({
     message: "Usuário cadastrado com sucesso",
