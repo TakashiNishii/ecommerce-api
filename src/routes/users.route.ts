55c25849223f65ef5e97dd279feef89f43be175a
TakashiNishii <ygortn14@gmail.com>
2025-01-20 17:46:28 -0300

    :wrench: CRUD with firestore

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,60 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { UsersController } from "../controllers/users.controller";
 
 export const userRoutes = express.Router()
 
-type User = {
-  id: number; 
-  nome: string; 
-  email: string;
-}
-
-let id = 0;
-let usuarios: User[] = [];
-
-userRoutes.get("/users", (req: Request, res: Response) => {
-  res.send(usuarios);
-})
-
-userRoutes.get("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let user = usuarios.find(user => user.id === userId);
-  res.send(user);
-})
-
-userRoutes.put("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let user = usuarios.find(user => user.id === userId);
-
-  if(user){
-    user.email = req.body.email;
-    user.nome = req.body.nome;
-    
-    res.send({
-      message: "Usuário atualizado com sucesso",
-    })
-  } 
-  if(!user) {
-    res.send({
-      message: "Usuário não encontrado",
-    })
-  }
-})
-
-userRoutes.delete("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let userList = usuarios.filter(user => user.id !== userId);
-  usuarios = userList;
-
-  res.send({
-    message: "Usuário deletado com sucesso",
-  })
-})
-
-userRoutes.post("/users", (req: Request, res: Response) => {
-  let user = req.body;
-  user.id = ++id;
-  usuarios.push(user);
-  res.send({
-    message: "Usuário cadastrado com sucesso",
-  })
-})
+userRoutes.get("/users", UsersController.getAll)
+userRoutes.get("/users/:id", UsersController.getById)
+userRoutes.post("/users", UsersController.save)
+userRoutes.put("/users/:id", UsersController.update)
+userRoutes.delete("/users/:id", UsersController.delete)
